3ed474b035edb57545c790671af9ab8fb396189a
Sig Fried <hyperboreanvrilcannibals@gmail.com>
2025-07-23 17:14:05 -0400

    Revert "Fix: Button size and Town page layout"
    This reverts commit 808d78a979a2bc4de11872f042e3d4a2020a2afa.

--- frontend/src/components/CenteredLayout.jsx
+++ frontend/src/components/CenteredLayout.jsx
@@ -28,20 +28,6 @@ const CenteredLayout = ({
   return (
     <div style={pageDynamicStyle} className="text-white overflow-hidden">
       <div className="flex flex-col w-full h-full max-w-6xl bg-gray-900 bg-opacity-80 rounded-xl shadow-2xl backdrop-blur-sm overflow-hidden p-6 items-center justify-center" style={{ maxHeight: '90vh' }}>
-        <div className="flex-1 p-6 sm:p-8 overflow-y-auto">
-          {error && (
-            <div className="bg-red-600 bg-opacity-90 border border-red-700 text-white p-3 mb-5 rounded-md shadow-lg">
-              <p className="font-semibold">Error: {error}</p>
-              <button
-                onClick={clearError}
-                className={`py-1 px-2 mt-2 text-xs rounded-md bg-red-500 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-red-300 focus:ring-opacity-75`}
-              >
-                Dismiss
-              </button>
-            </div>
-          )}
-          {renderContent()}
-        </div>
         <h1 className="text-3xl font-bold mb-6 text-yellow-400 text-center">{pageTitle}</h1>
         <div className="flex flex-col space-y-4 overflow-y-hidden overflow-x-hidden">
         {buttons.map((button, index) => (
@@ -55,6 +41,20 @@ const CenteredLayout = ({
             justify="center"
           />
         ))}
+        </div>
+        <div className="flex-1 p-6 sm:p-8 overflow-y-auto">
+          {error && (
+            <div className="bg-red-600 bg-opacity-90 border border-red-700 text-white p-3 mb-5 rounded-md shadow-lg">
+              <p className="font-semibold">Error: {error}</p>
+              <button
+                onClick={clearError}
+                className={`py-1 px-2 mt-2 text-xs rounded-md bg-red-500 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-red-300 focus:ring-opacity-75`}
+              >
+                Dismiss
+              </button>
+            </div>
+          )}
+          {renderContent()}
         </div>
       </div>
     </div>
